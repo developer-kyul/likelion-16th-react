5f58cfb1db8e774218ae0ee7bc294812e702e6e8
hankyul <developer.kyul@gmail.com>
2026-03-10 18:03:35 +0900

    learn :  랜더링

--- 03-렌더링-원리-불변성/src/app/index.tsx
+++ 03-렌더링-원리-불변성/src/app/index.tsx
@@ -1,9 +1,10 @@
-import S from './style.module.css'
+import { RenderPipeline } from "@/learns";
+import S from "./style.module.css";
 
 export default function App() {
   return (
     <div className={S.container}>
-
+      <RenderPipeline />
     </div>
-  )
+  );
 }
--- 03-렌더링-원리-불변성/src/learns/index.ts
+++ 03-렌더링-원리-불변성/src/learns/index.ts
@@ -2,17 +2,17 @@
 // 학습 순서
 // ---------------------------
 
-// 1. learn/RenderPipeline
-export { default as RenderPipeline } from './RednerPipeline'
+// 1. learn/RednerPipeline
+export { default as RenderPipeline } from "./RenderPipeline";
 
 // 2. learn/VirtualDOM
-export { default as VirtualDOM } from './VirtualDOM'
+export { default as VirtualDOM } from "./VirtualDOM";
 
 // 3. learn/Immutability
-export { default as Immutability } from './Immutability'
+export { default as Immutability } from "./Immutability";
 
 // 4. learn/ImmutabilityCRUD
-export { default as ImmutabilityCRUD } from './ImmutabilityCRUD'
+export { default as ImmutabilityCRUD } from "./ImmutabilityCRUD";
 
 // 5. learn/Reconciliation
-export { default as Reconciliation } from './Reconciliation'
+export { default as Reconciliation } from "./Reconciliation";
