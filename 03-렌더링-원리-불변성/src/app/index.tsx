99c13eddfcc2ceda1f48bf2a7b4c38f048465fa1
hankyul <developer.kyul@gmail.com>
2026-03-11 16:07:12 +0900

    chor. 주석 삭제

--- 03-렌더링-원리-불변성/src/app/index.tsx
+++ 03-렌더링-원리-불변성/src/app/index.tsx
@@ -1,19 +1,9 @@
-import {
-  Immutability,
-  ImmutabilityCRUD,
-  Reconciliation,
-  RenderPipeline,
-  VirtualDOM,
-} from "@/learns";
+import { Reconciliation } from "@/learns";
 import S from "./style.module.css";
 
 export default function App() {
   return (
     <div className={S.container}>
-      {/* <RenderPipeline /> */}
-      {/* <VirtualDOM /> */}
-      {/* <Immutability /> */}
-      {/* <ImmutabilityCRUD /> */}
       <Reconciliation />
     </div>
   );
