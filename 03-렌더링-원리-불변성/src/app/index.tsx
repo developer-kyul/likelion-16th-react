ba58bcbada0bf0e630a1268a84ad3f7056131118
hankyul <developer.kyul@gmail.com>
2026-03-12 10:18:08 +0900

    learn. 컴포넌트 초기화

--- 03-렌더링-원리-불변성/src/app/index.tsx
+++ 03-렌더링-원리-불변성/src/app/index.tsx
@@ -1,10 +1,15 @@
-import { Reconciliation } from "@/learns";
 import S from "./style.module.css";
+import { StateInitialization } from "@/practices";
 
 export default function App() {
   return (
     <div className={S.container}>
-      <Reconciliation />
+      {/* RenderPipeline */}
+      {/* VirtualDOM */}
+      {/* Immutability */}
+      {/* ImmutabilityCRUD */}
+      {/* Reconciliation */}
+      <StateInitialization />
     </div>
   );
 }
--- 03-렌더링-원리-불변성/src/practices/StateInitialization/index.tsx
+++ 03-렌더링-원리-불변성/src/practices/StateInitialization/index.tsx
@@ -1,5 +1,21 @@
-import S from './style.module.css'
+import S from "./style.module.css";
+import UserForm from "./UserForm";
 
-export default function App() {
-  return <div className={S.container}></div>
+// 컴포넌트 인스턴스는 key를 기반으로 갱신되고 재사용됩니다.
+// 인스턴스 = 설계도를 기반으로 생성된 객체
+// 갱신 = 업데이트
+
+export default function StateInitialization() {
+  return (
+    <div className={S.container}>
+      <header className={S.header}>
+        <h2>회원가입 (버전: {0})</h2>
+        <button type="button" className={S.resetButton}>
+          폼 초기화
+        </button>
+      </header>
+
+      <UserForm />
+    </div>
+  );
 }
