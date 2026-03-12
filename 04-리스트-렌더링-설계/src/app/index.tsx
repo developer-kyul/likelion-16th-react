4629ddf259120a7987848378ed76bff48330ed0d
hankyul <developer.kyul@gmail.com>
2026-03-12 14:12:54 +0900

    learn. 리스트 렌더링과 유니크한 Key의 중요성 - key = 리스트 아이템 간 고유한 식별자 - 항목의 순서가 바뀌어야 할 경우, 필히 고유 값 사용 (예: id 등) - map() 메서드의 index는 최후의 수단!!

--- 04-리스트-렌더링-설계/src/app/index.tsx
+++ 04-리스트-렌더링-설계/src/app/index.tsx
@@ -1,12 +1,13 @@
-import ProductPage from "@/practices/02-filtering/pages/product";
 import S from "./style.module.css";
-import { SimpleList, StaffList } from "@/components";
+import { ScheduleTable } from "@/components";
 
 export default function App() {
   return (
     <div className={S.container}>
       {/* <SimpleList /> */}
-      <StaffList />
+      {/* <StaffList /> */}
+      {/* <ShiftManager /> */}
+      <ScheduleTable />
     </div>
   );
 }
