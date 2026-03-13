677dd53fe353de8b9089c50dcdd768cc0c2b14d4
hankyul <developer.kyul@gmail.com>
2026-03-13 10:05:00 +0900

    learn : 리팩토링

--- 04-리스트-렌더링-설계/src/components/StaffListWithFilter/index.tsx
+++ 04-리스트-렌더링-설계/src/components/StaffListWithFilter/index.tsx
@@ -1,22 +1,17 @@
 import { useState } from "react";
-import StaffData from "./data/staff.json";
+import staffData from "./data/staff.json";
 import type { Staff } from "./type/staff";
-import S from "./style.module.css";
+import StaffListInfo from "./parts/StaffListInfo";
 import StaffListSearch from "./parts/StaffListSearch";
 import StaffList from "./parts/StaffList";
+import S from "./style.module.css";
 
 export default function StaffListWithFilter() {
-  const [staffs] = useState<Staff[]>(StaffData);
+  const [staffs] = useState<Staff[]>(staffData);
 
   return (
     <section className={S.container}>
-      <header className={S.header}>
-        <h2>알바생 관리 명부</h2>
-        <span className={S.count}>
-          검색된 인원: {staffs.length} / 총원: {staffs.length}명
-        </span>
-      </header>
-
+      <StaffListInfo staffs={staffs} />
       <StaffListSearch />
       <StaffList staffs={staffs} />
     </section>
