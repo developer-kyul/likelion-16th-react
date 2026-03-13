6fac1a9688eaba5ce981aeeefc84b0e23cae6459
hankyul <developer.kyul@gmail.com>
2026-03-13 16:14:29 +0900

    learn. 이펙트 종속성(Dependencies) - 종속성이 없는 경우 (항상 실행) - 종속성이 빈 경우 (최초 1회 실행) - 종속성에 반응성 데이터가 추가된 경우 (반응성 데이터가 변경된 경우에만 실행)

--- 05-이펙트-비동기-통신/src/app/index.tsx
+++ 05-이펙트-비동기-통신/src/app/index.tsx
@@ -1,10 +1,11 @@
-import { RaceCondition } from '@/components'
-import S from './style.module.css'
+import { EffectDependencies } from "@/components";
+import S from "./style.module.css";
 
 export default function App() {
   return (
     <div className={S.container}>
-      <RaceCondition />
+      {/* <EffectBasic /> */}
+      <EffectDependencies />
     </div>
-  )
+  );
 }
