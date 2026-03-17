546ea96be9b25730d5f6ef12936ded1fe323cd31
hankyul <developer.kyul@gmail.com>
2026-03-17 13:10:23 +0900

    practice. 사용자 ID로 할 일 검색 Part 2. - API 데이터 가져오기 함수 분리 (타입 포함) - 파생된 상태를 활용해 랜덤 완료 상태 목록 구성 (유틸리티 함수 사용)

--- 05-이펙트-비동기-통신/src/api/getPosts.ts
+++ 05-이펙트-비동기-통신/src/api/getPosts.ts
@@ -1,41 +1,21 @@
-const { VITE_API_URL } = import.meta.env;
+// API 엔드포인트(Endpoint)
+const { VITE_API_URL: API_URL } = import.meta.env;
 
-export interface Post {
+// 응답 데이터 타입 지정
+interface ResponseTodosData {
+  message: string;
+  todos: Todo[];
+}
+
+export interface Todo {
   id: number;
-  title: string;
   content: string;
-  imgUrl: string;
-  createdAt: string;
+  completed: boolean;
   userId: number;
 }
 
-interface ResponsePostsData {
-  hasNextPage: boolean;
-  limit: number;
-  message: string;
-  page: number;
-  posts: Post[];
-}
-
-export const getPosts = async ({
-  page = 1,
-  limit = 5,
-} = {}): Promise<ResponsePostsData> => {
-  console.log(`${VITE_API_URL}/api/posts?page=${page}&limit=${limit}`);
-
-  try {
-    const response = await fetch(
-      `${VITE_API_URL}/api/posts?page=${page}&limit=${limit}`,
-    );
-
-    if (!response.ok) {
-      // 응답이 성공하지 않은 경우, 에러를 생성해 전달합니다.
-      throw new Error("포스트 리스트 데이터 가져오기에 실패했습니다.");
-    }
-
-    return response.json();
-  } catch (error) {
-    // 에러가 던져지면, 이를 다시 던져서 이 함수를 호출한 쪽에서 캐치(catch)할 수 있게 합니다.
-    throw error instanceof Error ? error : new Error(String(error));
-  }
+export const getTodos = async (userId: string) => {
+  const response = await fetch(`${API_URL}/api/todos?userId=${userId}`);
+  const data: ResponseTodosData = await response.json();
+  return data.todos;
 };
