93244521f72ed22a360424dd40eeeb6403362831
hankyul <developer.kyul@gmail.com>
2026-03-19 11:36:17 +0900

    learn. useRef 훅 사용의 필요성 - 렌더링과 상관 없이 데이터 기억 - 로직에는 사용되지만, 화면에는 출력될 필요가 없는 값

--- 06-폼-핸들링-DOM-참조/src/components/RefStudy/RefStudy.tsx
+++ 06-폼-핸들링-DOM-참조/src/components/RefStudy/RefStudy.tsx
@@ -1,17 +1,20 @@
-import CounterComparisonClass from './parts/CounterComparison.class'
-import CounterComparison from './parts/CounterComparison'
-import TimerIdentifier from './parts/TimerIdentifier'
-import DomFocusControl from './parts/DomFocusControl'
-import S from './RefStudy.module.css'
+// import CounterComparisonClass from "./parts/CounterComparison.class";
+// import CounterComparison from "./parts/CounterComparison";
+import TimerIdentifier from "./parts/TimerIdentifier";
+import DomFocusControl from "./parts/DomFocusControl";
+import S from "./RefStudy.module.css";
 
 export default function RefStudy() {
   return (
     <div className={S.container}>
       <h2 className={S.mainTitle}>useRef 훅 함수 활용</h2>
-      <CounterComparisonClass />
-      <CounterComparison />
+      {/* useRef */}
+      {/* <CounterComparisonClass /> */}
+      {/* <CounterComparison /> */}
+
+      {/* 타이머 실습 */}
       <TimerIdentifier />
       <DomFocusControl />
     </div>
-  )
+  );
 }
