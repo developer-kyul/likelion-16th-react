923f7bea2757218b389163942eb417c8493eeac4
hankyul <developer.kyul@gmail.com>
2026-03-20 11:21:19 +0900

    add. 멀티 파일 업로드 (참고)

--- 06-폼-핸들링-DOM-참조/src/app/index.tsx
+++ 06-폼-핸들링-DOM-참조/src/app/index.tsx
@@ -1,10 +1,11 @@
-import { FileUpload } from '@/components'
-import S from './style.module.css'
+import { MultiFilesUpload } from "@/components";
+import S from "./style.module.css";
 
 export default function App() {
   return (
     <div className={S.container}>
-      <FileUpload />
+      {/* <FileUpload /> */}
+      <MultiFilesUpload />
     </div>
-  )
+  );
 }
