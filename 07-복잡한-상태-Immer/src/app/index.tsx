e40872288c66f8b9b37ee614e3d023e2b9c4df9c
hankyul <developer.kyul@gmail.com>
2026-03-20 13:11:26 +0900

    learn. 참조형 데이터와 불변성 (뮤테이션 ❌)

--- 07-복잡한-상태-Immer/src/app/index.tsx
+++ 07-복잡한-상태-Immer/src/app/index.tsx
@@ -1,9 +1,10 @@
-import S from './style.module.css'
+import { ReferenceCheck } from "@/components";
+import S from "./style.module.css";
 
 export default function App() {
   return (
     <div className={S.container}>
-      {/*  */}
+      <ReferenceCheck />
     </div>
-  )
+  );
 }
