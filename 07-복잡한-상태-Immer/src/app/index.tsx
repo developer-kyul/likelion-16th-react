adf5f434eda1c062a3191ded2169e8bef90773a1
hankyul <developer.kyul@gmail.com>
2026-03-20 15:10:00 +0900

    learn. [방법 1] 비제어 방식 - 명령형 프로그래밍으로 사용자가 직접 화면 제어

--- 07-복잡한-상태-Immer/src/app/index.tsx
+++ 07-복잡한-상태-Immer/src/app/index.tsx
@@ -1,10 +1,11 @@
-import { ReferenceCheck } from "@/components";
+import { TodosCRUD } from "@/components";
 import S from "./style.module.css";
 
 export default function App() {
   return (
     <div className={S.container}>
-      <ReferenceCheck />
+      {/* <ReferenceCheck /> */}
+      <TodosCRUD />
     </div>
   );
 }
