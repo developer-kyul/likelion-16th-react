e4beafada68942c262bfb836338ef02ce087e4d3
hankyul <developer.kyul@gmail.com>
2026-03-23 13:35:21 +0900

    learn.  장바구니(ShoppingCart) 실습 2.

--- 07-복잡한-상태-Immer/src/app/index.tsx
+++ 07-복잡한-상태-Immer/src/app/index.tsx
@@ -1,13 +1,9 @@
-import { ShoppingCart } from "@/practices";
+import ShoppingCart from "@/practices/ShoppingCart-step-3/ShoppingCart";
 import S from "./style.module.css";
 
 export default function App() {
   return (
     <div className={S.container}>
-      {/* <ReferenceCheck /> */}
-      {/* <TodosCRUD /> */}
-      {/* <TodosCrudWithImmer /> */}
-      {/* <DerivedState /> */}
       <ShoppingCart />
     </div>
   );
