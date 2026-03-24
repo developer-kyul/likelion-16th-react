7a89b779f3251d37e46d2f14b4e06bfbd3ec0c88
hankyul <developer.kyul@gmail.com>
2026-03-24 11:35:48 +0900

    learn. 성능 저하 진단 및 개선 상황 B - SmartHomePanel-step-2 폴더 - 상태 끌어내리기, 상태 업데이트 함수 활용 ~ 우선책 - useCallback, React.memo() ~ 차선책

--- 08-성능-개선-최적화/src/app/index.tsx
+++ 08-성능-개선-최적화/src/app/index.tsx
@@ -1,27 +1,10 @@
-import { SmartHomePanel } from "@/practices";
-import S from "./style.module.css";
-// import { CompositionRender } from "@/learns";
-// import GrandFather from "../learns/CompositonRender/parts/GrandFather";
-// import Father from "../learns/CompositonRender/parts/Father";
-// import Child from "../learns/CompositonRender/parts/Child";
-// import MemoizationValue from './../learns/MemoizationValue/index';
+import SmartHomePanel from '@/practices/SmartHomePanel-step-2'
+import S from './style.module.css'
 
 export default function App() {
   return (
     <div className={S.container}>
-      {/* <CompositionRender>
-        <GrandFather>
-          <Father>
-            <Child />
-          </Father>
-        </GrandFather>
-      </CompositionRender> */}
-
-      {/* <MemoizationRender /> */}
-      {/* <MemoizationCallback /> */}
-      {/* <MemoizationValue /> */}
-
       <SmartHomePanel />
     </div>
-  );
+  )
 }
