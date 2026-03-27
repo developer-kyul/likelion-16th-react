b96bec00c6a1c810dc81cd5db7c01f6e5abc435b
hankyul <developer.kyul@gmail.com>
2026-03-27 11:27:09 +0900

    add. 학습자료

--- 10-Context-API-전역-상태/src/app/index.tsx
+++ 10-Context-API-전역-상태/src/app/index.tsx
@@ -1,9 +1,13 @@
+import { ContextAdvanced } from '@/_/learns'
+import { FamilyProvider } from '@/contexts/FamilyContext/provider'
 import S from './style.module.css'
 
 export default function App() {
   return (
-    <div className={S.container}>
-      {/*  */}
-    </div>
+    <FamilyProvider>
+      <div className={S.container}>
+        <ContextAdvanced />
+      </div>
+    </FamilyProvider>
   )
 }
