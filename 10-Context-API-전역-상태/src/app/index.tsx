6dc3af249bec3be29544cb02efa590838ad9e829
hankyul <developer.kyul@gmail.com>
2026-03-27 14:35:04 +0900

    learn. 테마 컨텍스트 작성

--- 10-Context-API-전역-상태/src/app/index.tsx
+++ 10-Context-API-전역-상태/src/app/index.tsx
@@ -1,13 +1,17 @@
 import { ContextAdvanced } from '@/_/learns'
-import { FamilyProvider } from '@/contexts/FamilyContext/provider'
+import { AuthProvider, FamilyProvider, ThemeProvider } from '@/contexts'
 import S from './style.module.css'
 
 export default function App() {
   return (
-    <FamilyProvider>
-      <div className={S.container}>
-        <ContextAdvanced />
-      </div>
-    </FamilyProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <div className={S.container}>
+          <FamilyProvider>
+            <ContextAdvanced />
+          </FamilyProvider>
+        </div>
+      </ThemeProvider>
+    </AuthProvider>
   )
 }
