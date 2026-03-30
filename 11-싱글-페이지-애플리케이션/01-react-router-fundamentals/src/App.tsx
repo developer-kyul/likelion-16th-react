b3f59fda51698d72d72674d1197e62beeb448e6a
hankyul <developer.kyul@gmail.com>
2026-03-30 14:43:40 +0900

    learn. 로그아웃 후 홈 페이지로 이동 - 로그아웃 후, 이동이 아니라 그 반대로 처리! - 타이머를 사용해 잠깐의 시간 유지 후 로그아웃

--- 11-싱글-페이지-애플리케이션/01-react-router-fundamentals/src/App.tsx
+++ 11-싱글-페이지-애플리케이션/01-react-router-fundamentals/src/App.tsx
@@ -18,8 +18,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 */
 
+// 내비게이션 경로 (상수)
+import { NAVIGATION_PATH } from './configs/navigationPaths'
+
 // 컨텍스트 프로바이더
-import { MoviesProvider } from './contexts'
+import { AuthProvider, MoviesProvider } from './contexts'
 
 // 공용 레이아웃
 import CommonLayout from './layouts/CommonLayout'
@@ -31,26 +34,38 @@ import MovieDetail from './pages/MovieDetail'
 import MyPage from './pages/MyPage'
 import NotFound from './pages/NotFound'
 
+// 페이지 전환 시, 스크롤 위치를 페이지 상단으로 이동
+import ScrollToTop from './components/ScrollToTop'
+import ProtectedRoute from './components/ProtectedRoute'
+
 export default function App() {
   return (
-    <MoviesProvider>
+    // 인증 프로바이더(공급자)
+    <AuthProvider>
+      {/* 영화 프로바이더(공급자) */}
+      <MoviesProvider>
+        {/* 라우터 프로바이더(공급자) */}
       <BrowserRouter>
         <Routes>
           {/* 레이아웃 라우트 */}
-          <Route path="/" element={<CommonLayout />}>
+          <Route path={NAVIGATION_PATH.base} element={<CommonLayout />}>
             {/* 중첩된, 인덱스 라우트 */}
             <Route index element={<Home />} />
             {/* 경로(path)를 가지는 라우트 */}
-            <Route path="/login" element={<Login />} />
+            <Route path={NAVIGATION_PATH.login} element={<Login />} />
             {/* 다이내믹 세그먼트 라우트 */}
-            <Route path="/movies/:movieId" element={<MovieDetail />} />
+            <Route path={`${NAVIGATION_PATH.movies}/:movieId`} element={<MovieDetail />} />
             {/* 프로텍티드 라우트 */}
-            <Route path="/my" element={<MyPage />} />
+            <Route path={NAVIGATION_PATH.mypage} element={<ProtectedRoute><MyPage /></ProtectedRoute>} />
+            {/* 보호된 라우트 예시 (예: 배우들) */}
+            {/* <Route path={NAVIGATION_PATH.actors} element={<ProtectedRoute><div>배우들</div></ProtectedRoute>} /> */}
             {/* 와일드 카드 */}
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
+        <ScrollToTop />
       </BrowserRouter>
-    </MoviesProvider>
+      </MoviesProvider>
+    </AuthProvider>
   )
 }
